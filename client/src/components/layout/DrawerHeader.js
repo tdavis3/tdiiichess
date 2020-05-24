f16f13115ff5c117dc8c10da86d57132ccc8e01e
tdavis3 <tyronedavisci@gmail.com>
2020-05-24 15:18:56 -0400

    Add profile page and admin functionality

--- client/src/components/layout/DrawerHeader.js
+++ client/src/components/layout/DrawerHeader.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-import {makeStyles} from '@material-ui/core/styles';
+import {
+    CssBaseline,
+    MenuItem,
+    IconButton,
+    Box,
+    Grid,
+    Typography,
+    Popper,
+    Grow,
+    Paper,
+    ClickAwayListener,
+    MenuList
+} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
-import {CssBaseline, Menu, MenuItem, IconButton, Box, Grid, Typography} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
 
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {logout} from "../../actions/auth";
 
-
 const useStyles = makeStyles(theme => ({
     root: {
+        // display: 'flex',
         padding: theme.spacing(1)
     },
     logo: {
@@ -23,60 +35,91 @@ const useStyles = makeStyles(theme => ({
         fontSize: 14,
     },
     menu: {
-        display: 'flex',
+        // display: 'flex',
         alignItems: 'center'
-    }
+    },
 }));
 
-const DrawerHeader = (props) => {
+const DrawerHeader = ({auth, logout}) => {
     const classes = useStyles();
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
     };
 
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
     };
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
 
     return (
         <Box className={classes.root}>
             <CssBaseline/>
-            <Typography className={classes.logo}>TDIII Chess</Typography>
-            <Grid container spacing={3}>
+            <div className={classes.logo}>
+                <Typography>TDIII Chess</Typography>
+            </div>
+            <Grid container spacing={2}>
                 <Grid item xs={9}>
                     <Typography
-                        className={classes.user} noWrap>{props.first_name} {props.last_name}</Typography>
-                    <Typography className={classes.user} noWrap>{props.email}</Typography>
+                        className={classes.user} noWrap>{auth.user.first_name} {auth.user.last_name}</Typography>
+                    <Typography className={classes.user} noWrap>{auth.user.email}</Typography>
                 </Grid>
                 <Grid item xs={3} className={classes.menu}>
                     <IconButton
-                        aria-label={"Profile"}
-                        aria-controls={"profile-menu"}
-                        aria-haspopup={"true"}
-                        onClick={handleClick}
+                        ref={anchorRef}
+                        aria-controls={open ? 'menu-list-grow' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleToggle}
                     >
-                        <MenuIcon/>
+                        <MenuIcon fontSize={"medium"}/>
                     </IconButton>
-                    <Menu
-                        id="profile-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem component={Link} to={'/account'} onClick={handleClose}>My account</MenuItem>
-                        <MenuItem onClick={props.logout}>Logout</MenuItem>
-                    </Menu>
+                    <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                        {({TransitionProps, placement}) => (
+                            <Grow
+                                {...TransitionProps}
+                                style={{transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom'}}
+                            >
+                                <Paper>
+                                    <ClickAwayListener onClickAway={handleClose}>
+                                        <MenuList autoFocusItem={open} id="menu-list-grow"
+                                                  onKeyDown={handleListKeyDown}>
+                                            <MenuItem component={Link} to={'/account'}
+                                                      onClick={handleClose}>Profile</MenuItem>
+                                            <MenuItem onClick={logout}>Logout</MenuItem>
+                                        </MenuList>
+                                    </ClickAwayListener>
+                                </Paper>
+                            </Grow>
+                        )}
+                    </Popper>
                 </Grid>
             </Grid>
         </Box>
     );
-};
-
+}
 
 DrawerHeader.propTypes = {
     logout: PropTypes.func.isRequired,
--- client/src/components/user/Account.js
+++ client/src/components/user/Account.js
@@ -1,35 +1,65 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {CssBaseline, Drawer, Divider, List, ListItem, ListItemText} from '@material-ui/core';
+import {
+    CssBaseline,
+    Chip,
+    Drawer,
+    Divider,
+    Box,
+    Typography,
+    Container,
+    List,
+    ListItem,
+    ListItemText
+} from '@material-ui/core';
+import DrawerHeader from "../layout/DrawerHeader";
 
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
-const drawerWidth = 220;
+
+const drawerWidth = 260;
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
     },
-    appBar: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-    },
     drawer: {
+        display: "inline-block",
         width: drawerWidth,
-        flexShrink: 0,
+        flexShrink: 0
     },
     drawerPaper: {
         width: drawerWidth,
     },
-    // necessary for content to be below app bar
+    drawerContainer: {
+        overflow: 'auto',
+    },
     toolbar: theme.mixins.toolbar,
+    logo: {
+        width: '60px',
+    },
+    center: {
+        textAlign: 'center',
+        fontSize: 18
+    },
+    box: {
+        padding: theme.spacing(1)
+    },
     content: {
         flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
-        padding: theme.spacing(3),
+        height: '100vh',
+        overflow: 'auto',
     },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    profile: {
+        marginLeft: 100
+    }
 }));
 
 const Account = ({auth}) => {
@@ -45,19 +75,59 @@ const Account = ({auth}) => {
                 }}
                 anchor="left"
             >
-                <div className={classes.toolbar}/>
+                <DrawerHeader
+                    first_name={auth.user.first_name}
+                    last_name={auth.user.last_name}
+                    email={auth.user.email}
+                />
+                {/*TODO DrawerHeader buttons overlaps the menu buttons in main drawer*/}
                 <Divider/>
+                <Box className={classes.box}>
+                    <Typography className={classes.center}>Menu</Typography>
+                </Box>
                 <List>
-                    <ListItem button>
-                        <ListItemText primary={"Tournaments"}/>
+                    <ListItem button component={Link} to={'/tournaments'}>
+                        <ListItemText primary="Tournaments"/>
                     </ListItem>
+                    {auth.user.admin ? (
+                        <ListItem button component={Link} to={'/analytics'}>
+                            <ListItemText primary="Analytics"/>
+                        </ListItem>
+                    ) : (<></>)}
                 </List>
-                <Divider/>
+                <Box className={classes.box}>
+                    <Typography className={classes.center}>Profile</Typography>
+                </Box>
             </Drawer>
             <main className={classes.content}>
-                <h1>Hi {auth.isAuthenticated ? auth.user.firstname : null} </h1>
+                <Container className={classes.container}>
+                    <Typography variant={"h5"} style={{marginBottom: 20}}>Profile</Typography>
+                    <div className={classes.profile}>
+                        <div>
+                            <Typography
+                                style={{display: 'inline-flex', verticalAlign: "middle", marginRight: 8}}
+                                variant={"h6"}>{auth.isAuthenticated ? auth.user.first_name.concat(" ", auth.user.last_name) : null} </Typography>
+                            {
+                                auth.user.admin ? (
+                                    <Chip
+                                        style={{display: 'inline-flex', verticalAlign: "middle", marginLeft: 8}}
+                                        size={"small"}
+                                        label={auth.user.admin ? 'Admin' : 'Tournament Director'}
+                                        color={"secondary"}
+                                        variant={"outlined"}
+                                    />
+                                ) : ('')
+                            }
+                        </div>
+                        <div>
+                            <Typography>Email: {auth.user.email}</Typography>
+                            <Typography>Password: ********</Typography>
+                        </div>
+                    </div>
+                </Container>
             </main>
         </div>
+
     );
 };
 
