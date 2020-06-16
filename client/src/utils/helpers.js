8db084a02c523c678d0e5a720f4bdc820b024b90
tdavis3 <tyronedavisci@gmail.com>
2020-06-16 13:28:47 -0400

    Add better date renderer

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -6,11 +6,27 @@ moment().format();
 export function dateRenderer(startDate, endDate) {
     const start = moment(startDate);
     const end = moment(endDate);
-    return (
-        start.isSame(end) ?
-            start.format("MM/DD/YYYY") :
-            start.format("MM/DD/YYYY").concat(" - ", end.format("MM/DD/YYYY"))
-    );
+    const startMonth = start.format('MMM');
+    const endMonth = end.format('MMM');
+    const startDay = start.format('D');
+    const endDay = end.format('D');
+    const startYear = start.format('YYYY');
+    if (start.isSame(end, 'month') || start.isSame(end, 'day')) {  // Checks for same month and year
+        if (startDay === endDay) {  // Jan 12, 2022
+            return startMonth.concat(" ", startDay, ", ", startYear);
+        } else {  // Jan 12-13, 2022
+            return startMonth.concat(" ", startDay, "-", endDay, ", ", startYear);
+        }
+    } else if (start.isSame(end, 'year') && start.diff(end, 'month') !== 0) {  // Same year, but different month
+        // Jan 12 - Feb 13, 2022
+        return startMonth.concat(" ", startDay, " - ", endMonth, " ", endDay, ", ", startYear);
+    } else {
+        // Jan 15, 2022 - Jan 23, 2023  ||  Dec 15, 2022 - Jan 5, 2023
+        const formattedStart = start.format('MMM D, YYYY');
+        const formattedEnd = end.format('MMM D, YYYY');
+        return formattedStart.concat(' - ', formattedEnd);
+    }
+    return 'Error';
 }
 
 export function tournament_status(startDate, endDate) {
